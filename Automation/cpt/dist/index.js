bf6bd5c5e9bdb6f7ae3b292b98a6d0fbbf1deaf4
Ethan Kim <87978570+EthanKim8683@users.noreply.github.com>
2024-12-19 16:20:56 -0800

    Remove ignored files/folders

--- Automation/cpt/dist/index.js
+++
@@ -1,8 +0,0 @@
-import compile from "./runner-utils/compile";
-import execute from "./runner-utils/execute";
-import fs from "fs";
-await compile(["/opt/homebrew/bin/g++-14", "-o", "o", "demo.cpp"]);
-console.log(JSON.stringify(await execute(["./o"], {
-    stdin: fs.openSync("in.txt", "r"),
-    stdout: fs.openSync("out.txt", "w"),
-})));
--- Automation/cpt/dist/runners/c++.js
+++
@@ -1,9 +0,0 @@
-import compile from "../runner-utils/compile";
-import execute from "../runner-utils/execute";
-import tmp from "tmp-promise";
-import path from "path";
-export default async (codePath) => {
-    const { path: exePath } = await tmp.file();
-    await compile(["/opt/homebrew/bin/g++-14", "-o", exePath, codePath]);
-    return async (stdio) => execute([path.relative(process.cwd(), exePath)], stdio);
-};
