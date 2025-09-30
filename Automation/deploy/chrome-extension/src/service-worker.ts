1a9415e002aa3d32e1d5b6c1d8d08cdd6b802b0b
Ethan Kim <ethankim8683@gmail.com>
2025-09-30 09:55:17 -0700

    Use TypeScript

---
+++ Automation/deploy/chrome-extension/src/service-worker.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+// Send to CLI that the current page should be scraped
+
+chrome.action.onClicked.addListener((_tab) => {
+  console.log("CLICKED!!");
+});
---
+++ Automation/deploy/chrome-extension/webpack.config.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const src = path.join(__dirname, "src");
+
+module.exports = {
+  entry: Object.fromEntries(
+    fs
+      .globSync("**/*.{js,ts}", {
+        cwd: src,
+        exclude: ["node_modules"],
+      })
+      .map((file) => {
+        const { name } = path.parse(file);
+        return [name, path.join(src, file)];
+      })
+  ),
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /^node_modules$/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "**/*",
+          context: "public",
+        },
+      ],
+    }),
+  ],
+};
