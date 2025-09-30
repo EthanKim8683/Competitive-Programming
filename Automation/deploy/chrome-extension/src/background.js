9ba4a64085376920971192bb1cfdc7fcc4aa0df7
Ethan Kim <ethankim8683@gmail.com>
2025-09-30 02:05:38 -0700

    Basic NATS usage

--- Automation/deploy/chrome-extension/src/background.js
+++ Automation/deploy/chrome-extension/src/background.js
@@ -1,21 +1,32 @@
-'use strict';
+"use strict";
+
+import { wsconnect } from "@nats-io/nats-core";
 
 // With background scripts you can communicate with popup
 // and contentScript files.
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
+let nc;
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
+  if (request.type === "GREETINGS") {
+    const responseMessage = `Hi ${
+      sender.tab ? "Con" : "Pop"
     }, my name is Bac. I am from Background. It's great to hear from you.`;
 
+    const requestMessage = request.payload.message;
+    (async () => {
+      if (!nc) {
+        nc = await wsconnect({ servers: "ws://127.0.0.1:8684" });
+      }
+      nc.publish("cp.test", requestMessage);
+    })();
+
     // Log message coming from the `request` parameter
-    console.log(request.payload.message);
+    console.log(requestMessage);
     // Send a response message
     sendResponse({
-      message,
+      message: responseMessage,
     });
   }
 });
