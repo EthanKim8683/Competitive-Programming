49e0714339aebd79fcd1d7cd077d89d4a571bea9
Ethan Kim <ethankim8683@gmail.com>
2026-06-08 00:08:34 -0700

    Note design in TODO

--- Scaffolds/embed.go
+++
@@ -1,9 +0,0 @@
-package scaffolds
-
-import "embed"
-
-//go:embed Contest
-var Contest embed.FS
-
-//go:embed Problem
-var Problem embed.FS
--- Scaffolds/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/EthanKim8683/Competitive-Programming/Scaffolds
-
-go 1.26.3
